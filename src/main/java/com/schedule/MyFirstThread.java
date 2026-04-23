373faa26d080fefec13927124fc33dc7dce9fe61
Yash Kumar <yashkumarkanha@gmail.com>
2026-04-23 20:19:06 +0530

    Fix: Correct MyFirstThread code structure

--- src/main/java/com/schedule/MyFirstThread.java
+++ src/main/java/com/schedule/MyFirstThread.java
@@ -2,14 +2,13 @@ package com.schedule;
 
 public class MyFirstThread {
     public static void main(String[] args) {
-        System.out.println("Main thread start : " + Thread.currentThread().getName());
+        System.out.println("Main thread start: " + Thread.currentThread().getName());
 
         Thread worker = new Thread(() -> {
             System.out.println("Worker thread running: " + Thread.currentThread().getName());
-
-            try{
+            try {
                 Thread.sleep(1000);
-            } catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println("Worker thread finished");
